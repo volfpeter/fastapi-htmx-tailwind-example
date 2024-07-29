a87d23096c28519e11e8941665d2cf7dffcb19dc
Peter Volf <do.volfp@gmail.com>
2024-07-29 09:54:34 +0200

    nitpicking

--- app/database.py
+++ app/database.py
@@ -8,6 +8,6 @@ from motorhead import AgnosticClient, AgnosticDatabase
 def get_database() -> AgnosticDatabase:
     """Database provider dependency for the application."""
     mongo_connection_string = "mongodb://127.0.0.1:27017"
-    database_name = "smartnow-db"
+    database_name = "example-db"
     client: AgnosticClient = AsyncIOMotorClient(mongo_connection_string)
     return client[database_name]
